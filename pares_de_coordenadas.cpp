0cc13e8773c390ab16d0a401fa832ba30d54b413
Luis Chirinos <luisemilio2006@gmail.com>
2024-11-24 14:19:47 -0400

    punto 2 p4

--- pares_de_coordenadas.cpp
+++ pares_de_coordenadas.cpp
@@ -1,30 +1,34 @@
-#include <iostream>
-#include <limits>
+#include<iostream>
+#include<limits>
+#include<cmath>
 
 using namespace std;
 
-int lineas_cruzadas(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4) {
+int lineas_cruzadas(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4){
 
   double m1=(x2!=x1)?(y2-y1)/(x2-x1):numeric_limits<double>::infinity();
   double m2=(x4!=x3)?(y4-y3)/(x4-x3):numeric_limits<double>::infinity();
 
-  if(m1==m2){
+  if (m1==m2){
     return 0;
+  }
+
+  if(m1*m2==-1||(isinf(m1)&&m2==0)||(m1==0&&isinf(m2))){
+    return 1;
   }
 
   double b1=y1-m1*x1;
   double b2=y3-m2*x3;
-
   double x_interseccion=(b2-b1)/(m1-m2);
   double y_interseccion=m1*x_interseccion+b1;
 
   if (min(x1, x2)<=x_interseccion&&x_interseccion<=max(x1, x2)&&
       min(y1, y2)<=y_interseccion&&y_interseccion<=max(y1, y2)&&
       min(x3, x4)<=x_interseccion&&x_interseccion<=max(x3, x4)&&
-      min(y3, y4)<=y_interseccion&&y_interseccion<=max(y3, y4)) {
-    return 1;
-  } else {
-    return -1;
+      min(y3, y4)<=y_interseccion&&y_interseccion<=max(y3, y4)){
+    return -1; 
+  } else{
+    return -2; 
   }
 }
 
